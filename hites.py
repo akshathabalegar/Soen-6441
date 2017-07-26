d459f79ce0bbe3723184fa508e7c6a657df49ab4
Hitesh <30299080+hitesh10100@users.noreply.github.com>
2017-07-26 13:43:37 -0400

    Add files via upload

--- hites.py
+++ hites.py
@@ -1,18 +1,17 @@
+#-*- coding: utf-8 -*-
 from decimal import Decimal,getcontext
 from decimal import *
 import math
-
-
 #Sets decimal to 7 digits of precision
 getcontext().prec = 7
-
+print('**********CALCULATING EVERYTHING FROM SCRATCH**********')
 def caluclating_factorial(number_value):
     if number_value<1:
         return 1
     else:
         return number_value * caluclating_factorial(number_value-1)
 
-def calculating_pi(number_value):   #plouffbig
+def calculating_pi(number_value):     #Bailey?Borwein?Plouffe formula
     pi_value = Decimal(0)
     i = 0
     while i < number_value:
@@ -43,13 +42,10 @@ def calculating_cosine(theta_value):
 
 
 for j in range(1,25):
-    
     pi_value=calculating_pi(24)
     
 print("Value of PI would be: ")
 print(pi_value)
-
- 
 
 def samesign(a, b):
         return a * b > 0
@@ -75,11 +71,8 @@ def f(x1):
 x1 = Decimal(bisect(f, 1, 3))
 print("Value of ANGLE in RADIANS would be ")
 print(x1)
-     
-
 theta_value=Decimal(180*x1/pi_value)
-
-
+ 
 print("Value of ANGLE in DEGREEs would be ")
 print(theta_value)
 
@@ -90,16 +83,18 @@ print("Value of cosine would be: ")
 print((calculating_cosine(x1)))
 radius_value=input('Enter legal value of the RADIUS:-')
 radius_value = Decimal(radius_value)
-if radius_value > 100:
+if radius_value>100:
     raise ValueError('A very specific bad thing happened')
 
-linesegment_value=2*(radius_value)*(1-(calculating_cosine(x1))/2)
+linesegment_value=2*(radius_value)*(1-(calculating_cosine(x1/2)))
 print("Value of LINE SEGMENT l would be: ")
 print(linesegment_value)
+def valuepassfromscratch(a):
 
-
+  a=linesegment_value
+ 
 #for comparing
-print(math.sin(x1))
-print(math.cos(x1))
-d=Decimal(1-(math.cos(x1))/2)
-print((2*(radius_value)*d))
+# print(math.sin(x1))
+# print(math.cos(x1))
+# d=Decimal(1-(math.cos(x1/2)))
+# print((2*(radius_value)*d))
