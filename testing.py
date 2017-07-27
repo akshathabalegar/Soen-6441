0ed7a3c4348bf9f2d43a8870fe1791f02f7741ff
Hitesh <30299080+hitesh10100@users.noreply.github.com>
2017-07-27 17:05:54 -0400

    Add files via upload

--- testing.py
+++ testing.py
@@ -3,18 +3,27 @@ import decimal
 import math
 from decimal import Decimal,getcontext
 from decimal import *
+
+from xml.etree.ElementTree import Element,ElementTree, tostring
+from xml.etree import ElementTree as ET
+import xml.dom.minidom as MD  
+import xml.dom.minidom as MD   
+import pdb
+# #>>> a="a string"
+pdb.set_trace()
 print('**********CALCULATING EVERYTHING USING LIBRARY FUNCTIONS**********')
-radius_value=input('Enter legal value of the RADIUS:-')
-radius_value = Decimal(radius_value)
-#radius_value = (radius_value)
-if Decimal(radius_value)>100:
-    raise ValueError('A very specific bad thing happened')
+
+try:   #EXCEPTION HANDLING
+    radius_value=Decimal(input('Enter the Legal value of RADIUS:'))
+except ValueError:
+    print ("Please enter the valid INPUT")
+    
 print('Value of PI using library functions is ')
 print(math.pi)
 def samesign(a, b):
         return a * b > 0
 
-def bisect(func, low, high):
+def bisect(func, low, high):    #The Bisection Method for calculating ROOT of the equation from SCRATCH
     'Find root of continuous function where f(low) and f(high) have opposite signs'
 
     assert not samesign(func(low), func(high))
@@ -50,6 +59,13 @@ d=Decimal(1-(math.cos(x1/2)))
 print('Value of length segment using library functions')
 print ((2*(radius_value)*d))
 
-def valuepassfromlibrary(l):
+root=Element('Output')
+tree=ElementTree(root)
+alphaValue=Element('Alpha')
+root.append(alphaValue)
+lengthValue=Element('Length')
+root.append(lengthValue)
+alphaValue.text='Value of Angle in DEGREE is: ' + str(theta_value)
+lengthValue.text='Value of LENGTH is : '+ str((2*(radius_value)*d))
+tree.write(open('pojectoutput123.xml', 'wb'))
 
-     l=Decimal(2*(radius_value)*d)
